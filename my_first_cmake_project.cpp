27012240f74e441fbe433d43005285750676613c
wakuwaku01 <wakuwaku.post.mail@gmail.com>
2023-03-05 20:24:03 +0300

    first commit

---
+++ my_first_cmake_project.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::string name;
+    std::cout << "Введите имя: ";
+    std::cin >> name;
+    std::cout << "Здравствуйте, " << name << "!" << std::endl;
+}
